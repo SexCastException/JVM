323cc5e88853116d5554b72417246d8dfd30a5d8
pyh <pyh@qq.com>
2019-07-27 18:44:43 +0800

    添加注释

--- jvm-example/src/com/huazai/classloader/Test18.java
+++ jvm-example/src/com/huazai/classloader/Test18.java
@@ -2,11 +2,11 @@ package com.huazai.classloader;
 
 public class Test18 {
     public static void main(String[] args) {
-        // 启动加载器所加载类所在目录的环境变量
+        // 启动类加载器所加载类所在目录的环境变量
         System.out.println(System.getProperty("sun.boot.class.path"));
-        // 扩展加载器所加载类所在目录的环境变量
+        // 扩展类加载器所加载类所在目录的环境变量
         System.out.println(System.getProperty("java.ext.dirs"));
-        // 应用加载器所加载类所在目录的环境变量
+        // 应用类加载器所加载类所在目录的环境变量
         System.out.println(System.getProperty("java.class.path"));
     }
 }
