1f5059a73349fa5e0bc799eb621172ac3abcf158
pyh <pyh@qq.com>
2019-07-29 00:04:13 +0800

    ServiceLoader方法使用以及源码解析

---
+++ jvm-example/src/com/huazai/classloader/Test26.java
@@ -0,0 +1,23 @@
+package com.huazai.classloader;
+
+import java.sql.Driver;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * ServiceLoader的使用参考该类文档
+ */
+public class Test26 {
+    public static void main(String[] args) {
+        // 重点查看方法以及相关的一系列相关源码
+        ServiceLoader<Driver> serviceLoader = ServiceLoader.load(Driver.class);
+        Iterator<Driver> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            Driver next = iterator.next();
+            System.out.println("class:" + next.getClass() + ",classLoader:" + next.getClass().getClassLoader());
+        }
+        System.out.println("--------------------");
+        System.out.println("当前上下文类加载器：" + Thread.currentThread().getContextClassLoader());
+        System.out.println("ServiceLoader的类加载器：" + ServiceLoader.class.getClassLoader());
+    }
+}
