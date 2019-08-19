f6565c5517d819b13e8083aef6e8a4fce7d471b2
pyh <pyh@qq.com>
2019-08-20 00:07:15 +0800

    note

--- jvm-example/src/com/huazai/loading/Test5.java
+++ jvm-example/src/com/huazai/loading/Test5.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 /**
  * 一个接口在初始化的时候，并不要求父接口都完成了初始化。
- * 只有在真正使用到接口的时候（如引用接口中定义的常量时），才会初始化。
+ * 只有在真正使用到接口的时候（如引用接口中定义的非编译器确定的常量时），才会初始化。
  */
 public class Test5 {
     public static void main(String[] args) {
