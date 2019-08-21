a81f0f9c8058b8fd4cfd0a290c422fa2f1be0b74
pyh <pyh@qq.com>
2019-08-21 23:57:53 +0800

    note

--- jvm-example/src/com/huazai/classloader/MyCat.java
+++ jvm-example/src/com/huazai/classloader/MyCat.java
@@ -3,5 +3,12 @@ package com.huazai.classloader;
 public class MyCat {
     public MyCat() {
         System.out.println("MyCat" + MyCat.class.getClassLoader());
+        /*
+            删除classpath的MySample的class文件后去掉该行注释，并在classpath保留MyCat的class文件，变为Mycat由系统加载器加载，
+            MySample由自定义加载器加载（父加载器为系统类加载）
+
+            系统类加载器加载MyCat类文件后，由于父加载器对子加载器不可见，系统加载器尝试加载MySample的class文件的时候找不到MySample的class文件，报错
+         */
+//        System.out.println("MySample+" + MySample.class.getClassLoader());
     }
 }
--- jvm-example/src/com/huazai/classloader/MySample.java
+++ jvm-example/src/com/huazai/classloader/MySample.java
@@ -1,6 +1,8 @@
 package com.huazai.classloader;
 
 /**
+ * 重点复习：
+ * 结论：
  * 关于类加载器命名空间：
  * 1、子类所加载的类能够访问父加载器所加载的类
  * 2、父加载器所加载的类无法访问到子加载器所加载的类
@@ -16,6 +18,10 @@ public class MySample {
  * 疑问：为什么把MyCat和MySample定义在同一个文件会报错呢
  */
 
+/*
+    操作：把MyCat类的class文件复制一份到桌面并删除classpath目录下的MySampple文件，在classpath保留MySample的class文件，或者二者相反，
+    一次验证以上结论.
+ */
 class Test17 {
     public static void main(String[] args) throws Exception {
         MyClassLoader16 classLoader = new MyClassLoader16("myClassLoader");
